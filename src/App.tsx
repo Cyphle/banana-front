a01e162f5f6d8cf74233d466c53b860303c95bb0
Cyphle <cyril.phamle@gmail.com>
2024-03-05 20:53:54 +0100

    some basic setup and tests

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
+import { Menu } from './shared/menu/Menu.tsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Menu></Menu>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={ viteLogo } className="logo" alt="Vite logo"/>
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={ reactLogo } className="logo react" alt="React logo"/>
         </a>
       </div>
       <h1 className="text-3xl font-bold underline">
         Vite + React
       </h1>
+      <p className="text-3xl text-gray-600">
+        Getting a new business off the ground is a lot of hard work.
+        Here are five ideas you can use to find your first customers.
+      </p>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={ () => setCount((count) => count + 1) }>
+          count is { count }
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <>
+      Bienvenue sur Banana ! Simplifiez vous la gestion de vos comptes.
+    </>
+  )
+}
