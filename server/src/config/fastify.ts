305e180eace7d59117d9a1cb224ae7c50da700c0
Cyphle <cyril.phamle@gmail.com>
2024-03-10 19:13:00 +0100

    server

--- server/src/config/fastify.ts
+++ server/src/config/fastify.ts
@@ -1,7 +1,23 @@
-import fastify, { FastifyInstance } from 'fastify';
+import fastify, { FastifyInstance, FastifyPluginOptions } from 'fastify';
 
-export const initFastify = (): FastifyInstance => {
-  return fastify({
+export const initFastify = (
+  decorators: ((fastify: FastifyInstance) => void)[] = [],
+  plugins: { plugin: (fastify: FastifyInstance, options: FastifyPluginOptions) => void, routesPrefix: string }[],
+): FastifyInstance => {
+  const server = fastify({
     logger: true
   });
+
+  decorators.forEach(decorator => decorator(server));
+
+  plugins.forEach(plugin => {
+    server.register(
+      plugin.plugin,
+      {
+        prefix: plugin.routesPrefix
+      }
+    );
+  });
+
+  return server;
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,11 @@
-import { FastifyReply } from 'fastify'
 import { accountPlugin } from './plugins/account/account.plugin';
-import { decoreWithUser, FastifyRequestWithUser } from './authentication';
+import { decoreWithUser } from './authentication';
 import { initFastify } from './config/fastify';
 
-const fastify = initFastify();
-
-decoreWithUser(fastify);
-
-fastify.register(
-  accountPlugin,
-  {
-    prefix: '/accounts'
-  }
+const fastify = initFastify(
+  [decoreWithUser],
+  [{ plugin: accountPlugin, routesPrefix: '/accounts' }]
 );
-
-// Some example
-fastify.get('/ping', async (request: FastifyRequestWithUser, reply: FastifyReply) => {
-  return `pong user: ${ request.user }`
-})
 
 const start = async () => {
   try {
