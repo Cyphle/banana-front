a4a715445fd8a4175a23882e4565762f8cbc0b41
Cyphle <cyril.phamle@gmail.com>
2024-07-01 17:23:04 +0200

    profile controller

--- server/src/plugins/profile/profile.controller.ts
+++ server/src/plugins/profile/profile.controller.ts
@@ -12,7 +12,7 @@ export const getMyProfile = (fastify: FastifyInstance): void => {
 
 export const getMyProfileExample = (fastify: FastifyInstance): void => {
   fastify.get('/', (request: CustomFastifyRequest, reply: FastifyReply) => {
-    console.log(request.database);
+    console.log(request.database); // TODO ça devrait être un middleware ça et comme ça on peut tester indépendamment
     reply
       .code(200)
       .header('Content-Type', 'application/json; charset=utf-8')
