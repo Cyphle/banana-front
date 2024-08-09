42343910f41aeb99f4022389e35d5b9df2521d51
Cyphle <cyril.phamle@gmail.com>
2024-08-09 13:59:43 +0200

    delete old

--- oldvite/src/stores/accounts/accounts.queries.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery, UseQueryResult } from '@tanstack/react-query';
-import { getAccounts } from '@/services/accounts.service.ts';
-
-export const useFetchAccounts = (): UseQueryResult => {
-  return useQuery({
-    queryKey: ['accounts'],
-    queryFn: getAccounts
-  });
-}
