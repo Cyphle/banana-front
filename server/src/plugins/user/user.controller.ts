a215e81a58bacd48752a68a51fb6f8a848ffd665
Cyphle <cyril.phamle@gmail.com>
2024-10-29 11:19:01 +0100

    set user context on session

--- server/src/plugins/user/user.controller.ts
+++ server/src/plugins/user/user.controller.ts
@@ -6,6 +6,11 @@ import { UserInfo } from './user.types';
 export const userInfoController = (handler: (database: Database) => (username: string) => UserInfo | undefined) => (fastify: FastifyInstance): void => {
   fastify.get('/info', (request: CustomFastifyRequest, reply: FastifyReply) => {
     const connectedProfile = request.session.get('user');
+
+    if (connectedProfile === undefined) {
+      reply.code(403);
+    }
+
     const info = handler(request.database!!)(connectedProfile.username);
 
     if (!!info) {
